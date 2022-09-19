d2cb2e765416be8ae7a10713a3ab49cd0628bd0f
Farid-T <startmob@gmail.com>
2022-09-19 14:31:15 +0200

    mise a jour

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+
 var ville = "Cambrai"
 const hypotenuse = 8
-var admission ="vrai"
-var
+var admission ="true"
+var nom ="BALBOA"
+var prénom ="Rocky"
+var age ="45"
+var pommes = 8
+var peches = 6
+var totalfruits = peches + pommes
+console.log(totalfruits)
